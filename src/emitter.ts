4e94546badd0de75337d1d27596c95cfbca5e3b7
hilezi <pleasurazy@gmail.com>
2020-04-20 03:03:12 +0800

    refactor: typed emittery

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,6 +1,9 @@
 import Emittery from 'emittery'
 
-export const emitter = new Emittery()
+export const emitter = new Emittery.Typed<
+  {},
+  'ready' | 'onPing' | 'settingChange'
+>()
 
 export enum Events {
   ready = 'ready',
