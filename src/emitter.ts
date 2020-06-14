b4f1b006e9803301f80e9c35c7607de834ab5f4a
hilezir <pleasurazy@gmail.com>
2020-06-14 16:05:24 +0800

    refactor: 標記出 events 的 emit 時機，協助 debug

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import Emittery from 'emittery'
 import { debugAPI } from '@/debugAPI'
 
@@ -14,3 +15,19 @@ export const emitter = new Emittery.Typed<
   Record<string, any>,
   keyof typeof Events
 >()
+
+// 使每個 emitter 之 events 的接受端受到 emit 觸發時，皆會記錄在 console
+emitter.on = new Proxy(emitter.on.bind(emitter), {
+  apply(target, key, receiver: [Events, (...args) => any]) {
+    if (typeof receiver[1] === 'function') {
+      receiver[1] = new Proxy(receiver[1], {
+        apply(listenserTarget, listenserKey, listenserReceiver) {
+          debugAPI.log.extend('events')(receiver[0], receiver[1].name)
+          return Reflect.apply(listenserTarget, listenserKey, listenserReceiver)
+        },
+      })
+    }
+
+    return Reflect.apply(target, key, receiver)
+  },
+})
