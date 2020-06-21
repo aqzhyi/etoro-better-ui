72dae10a6ab0f7d226b9bdcc240d8c7e0e2cd897
hilezir <pleasurazy@gmail.com>
2020-06-21 13:41:05 +0800

    refactor: 使 emitter.once 也有 debugAPI 提示訊息在 console 之中

--- src/emitter.ts
+++ src/emitter.ts
@@ -34,3 +34,25 @@ emitter.on = new Proxy(emitter.on.bind(emitter), {
     return Reflect.apply(target, key, receiver)
   },
 })
+
+// 使每個 emitter 之 events 的接受端受到 emit 觸發時，皆會記錄在 console
+emitter.once = new Proxy(emitter.once.bind(emitter), {
+  apply(onceFn, key, receiver: [Events, (...args) => any]) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const newOnceFn = Reflect.apply(onceFn, key, receiver)
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    newOnceFn['then'] = new Proxy(newOnceFn['then'], {
+      apply: (
+        listenserTarget,
+        listenserKey,
+        listenserReceiver: [(...args) => unknown],
+      ) => {
+        debugAPI.events(receiver[0], listenserReceiver[0].name)
+        return Reflect.apply(listenserTarget, listenserKey, listenserReceiver)
+      },
+    })
+
+    return newOnceFn
+  },
+})
