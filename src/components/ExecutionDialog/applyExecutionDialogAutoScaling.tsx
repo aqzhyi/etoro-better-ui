f7919aa141a5339d7c6110bc89ebbeb3bfe87804
hilezir <pleasurazy@gmail.com>
2020-06-27 09:07:02 +0800

    feat: 下單視窗會在螢幕高度不夠時，自動縮小，當你的螢幕高度過小時，應該會很適合

---
+++ src/components/ExecutionDialog/applyExecutionDialogAutoScaling.tsx
@@ -0,0 +1,30 @@
+import { GM } from '@/GM'
+
+/**
+ * Auto-scaling the execution dialog window to suitable who using the smaller screen
+ */
+GM.addStyle(`
+  @media (max-height:755px) {
+    .uidialog-content {
+      transform: scale(0.9);
+    }
+  }
+
+  @media (max-height:655px) {
+    .uidialog-content {
+      transform: scale(0.7);
+    }
+  }
+
+  @media (max-height:555px) {
+    .uidialog-content {
+      transform: scale(0.6);
+    }
+  }
+
+  @media (max-height:455px) {
+    .uidialog-content {
+      transform: scale(0.4);
+    }
+  }
+`)
