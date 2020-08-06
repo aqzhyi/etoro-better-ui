586c132a0a0dc55d0dcc3ca355ddb1f207455cea
hilezir <pleasurazy@gmail.com>
2020-08-07 06:06:52 +0800

    refactor: improve ProfitText, more options to customize how display

--- src/components/ProfitText.tsx
+++ src/components/ProfitText.tsx
@@ -1,23 +1,88 @@
+import { GM } from '@/GM'
+import { toCurrency } from '@/toCurrency'
 import React from 'react'
 
 export const ProfitText: React.FC<{
-  /** allow be negative number */
+  /**
+    allow be negative number
+
+    @example
+    777.771257325
+
+    // $777.77
+
+    @example
+    -777.771257325
+
+    // -$777.77
+  */
   profit: number
+  /**
+    Define component present for either as Pure Dollar Display
+
+    @example
+    false | undefined
+
+    // present as green color when value >= 0
+
+    // present as red color when value < 0
+
+    @example
+    true
+
+    // present either as not green but also not red
+  */
+  pureDollar?: boolean
+  /**
+    Define component present a Dollar Sign as a prefix
+
+    @example
+    false
+
+    // 777.77
+
+    @example
+    true
+
+    // $777.77
+   */
+  noDollarSign?: boolean
 }> = props => {
   const isNegative = props.profit < 0
-  const profit = Math.abs(props.profit)
+  const values = toCurrency(Number(props.profit.toFixed(2)))
 
-  const dollarPrefix = isNegative ? '-$' : '$'
+  const dollarPrefix = props.noDollarSign
+    ? isNegative
+      ? '-'
+      : ''
+    : isNegative
+    ? '-$'
+    : '$'
 
   const styles: React.CSSProperties = {
-    color: !isNegative ? '#6eaf0f' : '#e1191d',
+    color:
+      props.pureDollar === true
+        ? 'orange'
+        : !isNegative
+        ? '#6eaf0f'
+        : '#e1191d',
     fontWeight: 'bold',
   }
 
   return (
     <span style={styles}>
-      {dollarPrefix}
-      {profit.toFixed(2)}
+      <span className={`${ProfitText.name}__dollarPrefix`}>{dollarPrefix}</span>
+      <span className={`${ProfitText.name}__valueMain`}>
+        {isNegative}
+        {values[0]}
+      </span>
+      <span className={`${ProfitText.name}__valueSmall`}>.{values[1]}</span>
     </span>
   )
 }
+
+GM.addStyle(`
+  .${ProfitText.name}__valueSmall {
+    font-size: 0.75em;
+  }
+`)
