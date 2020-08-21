e2ac5bc9c63b035746b38811cad7693c81b06b9f
hilezir <pleasurazy@gmail.com>
2020-08-21 14:03:29 +0800

    refactor: fix data, should reverse enabled

--- src/components/Sidebar/SidebarSettingsDialog.tsx
+++ src/components/Sidebar/SidebarSettingsDialog.tsx
@@ -124,7 +124,7 @@ export const SidebarSettingsDialog: React.FC = () => {
 
               dispatch(
                 setBetterEtoroUIConfig({
-                  executionMacroEnabledInProchart: enabled,
+                  executionMacroEnabledInProchart: !enabled,
                 }),
               )
             }}
