cf785f5afae9f0bc67489b86912ad77b9a98fe1c
hilezir <pleasurazy@gmail.com>
2020-09-11 13:54:58 +0800

    feat(TradeDashboard): revert to the version which Headers should display

--- src/components/InstrumentPositionGroupListItem.tsx
+++ src/components/InstrumentPositionGroupListItem.tsx
@@ -1,13 +1,42 @@
+import { ListItem, ListItemText } from '@material-ui/core'
 import { map } from 'lodash'
 import React, { Fragment } from 'react'
 import { InstrumentPosition } from '~/angularAPI'
 import { InstrumentPositionListItem } from '~/components/InstrumentPositionListItem'
+import { PrimaryTrans } from '~/components/PrimaryTrans'
 
 export const InstrumentPositionGroupListItem: React.FC<{
   positionIds?: InstrumentPosition['PositionID'][]
 }> = props => {
   return (
     <Fragment>
+      <ListItem>
+        <ListItemText
+          primary={
+            <PrimaryTrans i18nKey='tradeDashboard_amount'></PrimaryTrans>
+          }
+        ></ListItemText>
+
+        <ListItemText
+          primary={<PrimaryTrans i18nKey='tradeDashboard_rates'></PrimaryTrans>}
+        ></ListItemText>
+
+        <ListItemText
+          primary={<PrimaryTrans i18nKey='tradeDashboard_SLTP'></PrimaryTrans>}
+        ></ListItemText>
+
+        <ListItemText
+          primary={
+            <PrimaryTrans i18nKey='tradeDashboard_profit'></PrimaryTrans>
+          }
+        ></ListItemText>
+
+        <ListItemText
+          primary={
+            <PrimaryTrans i18nKey='tradeDashboard_action'></PrimaryTrans>
+          }
+        ></ListItemText>
+      </ListItem>
       {map(props.positionIds, id => {
         return <InstrumentPositionListItem key={id} positionId={id} />
       })}
