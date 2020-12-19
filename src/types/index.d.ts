59c3fc46176829f9527599a579144cd296f3fcea
hilezir <pleasurazy@gmail.com>
2020-12-19 11:12:11 +0800

    chore: typesRoot for *.d.ts

---
+++ src/types/index.d.ts
@@ -0,0 +1,28 @@
+const { registeredComponents } = await import('~/utils/registerReactComponent')
+const { store } = await import('~/store/_store')
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'test' | 'production' | 'development'
+  }
+}
+
+declare namespace globalThis {
+  const unsafeWindow: Window & {
+    __BETTER_ETORO_UI__: {
+      registeredComponents: typeof registeredComponents
+      store: typeof store
+    }
+  }
+}
+
+interface NodeModule {
+  hot?: {
+    accept?(dep, callback)
+  }
+}
+
+declare module '*.svg' {
+  const component: React.FC
+  export default component
+}
---
+++ src/types/refs.d.ts
@@ -0,0 +1,5 @@
+/**
+  This line to support the @emotion v11
+  see https://emotion.sh/docs/emotion-11
+ */
+/// <reference types="@emotion/react/types/css-prop" />
