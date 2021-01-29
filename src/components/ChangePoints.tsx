0be6ccd17a8b51d8ad6b598363eaa93dbbbaafca
hilezir <pleasurazy@gmail.com>
2021-01-29 21:22:58 +0800

    fix(TradeDialog): wrong value of change points on stocks, BTC, ETF, etc (was working on FX only)

--- src/components/ChangePoints.tsx
+++ src/components/ChangePoints.tsx
@@ -8,7 +8,24 @@ export const ChangePoints = memo<
     className?: string
   }>
 >(function ChangePoints(props) {
-  const value = Big(1).plus(Big(props.value).times(1000000)).minus(1).div(10)
+  if (props.value === 0) {
+    return <span></span>
+  }
 
-  return <span className={props.className}>{value.toNumber()}</span>
+  const valueAsString = props.value.toString().split('.')
+  const precision = valueAsString?.[1]?.length ?? 0
+  const integerValue = Number(valueAsString[0])
+  const withPrecision = Big(10).pow(precision - 1)
+
+  const value =
+    integerValue >= 1
+      ? Big(Number(valueAsString[0]))
+      : Big(Number(`${valueAsString[0]}${valueAsString[1]}`))
+
+  const changePoints =
+    integerValue >= 1
+      ? value
+      : Big(1).plus(value.times(withPrecision)).minus(1).div(withPrecision)
+
+  return <span className={props.className}>{changePoints.toNumber()}</span>
 })
